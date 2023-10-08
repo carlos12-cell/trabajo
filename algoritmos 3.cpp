aa392755d3c32de43365babf27ef167de98d58fb
carlos12-cell <147265787+carlos12-cell@users.noreply.github.com>
2023-10-07 18:04:52 -0600

    primer_inicio

---
+++ algoritmos 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main(){
+	//while
+	char s;
+	do{
+		cout<<"_______hacer algo:"<<endl;
+		cout<<"desea ingresar otro valor (s/n):";
+		cin>>respuesta;
+	
+	}while(respuesta=="s")
+	
+	
+	
+	system("pause");
+	
+	}
+	
---
+++ clase 23.1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+	//arreglos
+	char nombre;
+	cout<<"Ingrese nombre: ";
+	cin>>nombre;
+	cout<<nombre<<endl;
+}
